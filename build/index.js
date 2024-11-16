458df520f4953fab901d22e4acf936f598aa73ac
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-11-16 22:11:26 +0000

    Found holes and boundaries

--- build/index.js
+++ build/index.js
@@ -16,6 +16,10 @@ function convert_to_grayscale(path) {
             .grayscale()
             .raw()
             .toBuffer({ resolveWithObject: true });
+        // for (let i=0; i < buffer.info.size; i++) {
+        //     buffer.data[i] /= 255.0;
+        //     console.log(buffer.data[i]);
+        // }
         return buffer;
     });
 }
@@ -27,6 +31,9 @@ function processImage(imagePath, maskPath) {
         const height = image.info.height;
         const channels = image.info.channels;
         const filled_image = Object.assign({}, image);
+        // for (let i=0; i < filled_image.info.size; i++) {
+        //     console.log(filled_image.data[i]);
+        // }
         // Apply the mask!!
         let holes = [];
         for (let r = 0; r < width; r++) {
@@ -34,17 +41,78 @@ function processImage(imagePath, maskPath) {
                 const index = (r * width) + c;
                 const maskValue = mask.data[index];
                 if (maskValue < 0.5) {
-                    filled_image.data[index] = 0;
-                    holes.push([(r * width), c]);
+                    // filled_image.data[index] = -1;
+                    filled_image.data[index] = 200;
+                    holes.push([r, c, index]);
                 }
             }
         }
-        console.log(holes.length);
-        // Write out the image
+        // console.log(holes);
+        // Find the boundaries
+        const boundaries = new Set();
+        const neighbours = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+        for (let hole of holes) {
+            for (let neighbour of neighbours) {
+                const nr = hole[0] + neighbour[0];
+                if (nr < 0 || nr >= width) {
+                    continue;
+                }
+                const nc = hole[1] + neighbour[1];
+                if (nc < 0 || nc >= height) {
+                    continue;
+                }
+                if (mask.data[(nr * width) + nc] >= 0.5) {
+                    filled_image.data[(nr * width) + nc] = 0;
+                    boundaries.add([nr, nc, filled_image.data[(nr * width) + nc]]);
+                }
+            }
+        }
+        console.log(boundaries.size);
+        // // Write out the image
+        // for (let i=0; i < filled_image.data.length; i++) {
+        //     if (filled_image.data[i] == -1) {
+        //         console.log(i)
+        //     }
+        // }
         yield sharp(filled_image.data, { raw: { width, height, channels } })
             .toFormat('png')
             .toFile('.\\resources\\nature_filled.png');
     });
 }
+function test() {
+    let arr = [[1, 1, 1, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]];
+    let holes = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === 0) {
+                holes.push([i, j]);
+            }
+        }
+    }
+    console.log(holes);
+    let boundaries = new Set();
+    const neighbours = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for (let hole of holes) {
+        for (let neighbour of neighbours) {
+            const nr = hole[0] + neighbour[0];
+            if (nr < 0 || nr >= arr.length) {
+                continue;
+            }
+            const nc = hole[1] + neighbour[1];
+            if (nc < 0 || nc >= arr[0].length) {
+                continue;
+            }
+            if (arr[nr][nc] != 0) {
+                boundaries.add([nr, nc, arr[nr][nc]]);
+            }
+        }
+    }
+    console.log(boundaries);
+}
 console.log("Hello World!");
 processImage(".\\resources\\nature.png", ".\\resources\\nature_mask.png");
+// test();
--- src/index.ts
+++ src/index.ts
@@ -25,13 +25,36 @@ async function processImage(imagePath: string, maskPath: string) {
             const index = (r*width) +c;
             const maskValue = mask.data[index];
             if (maskValue < 0.5) {
-                filled_image.data[index] = 0;
-                holes.push( [(r*width), c] ); 
+                filled_image.data[index] = 200; // debug
+                holes.push( [r, c, index] ); 
+            }
+        }
+    }
+    // console.log(holes);
+
+    // Find the boundaries
+    const boundaries = new Set();
+    const neighbours = [[-1,0], [1,0], [0,-1], [0,1]];
+    for (let hole of holes) {
+        for (let neighbour of neighbours) {
+            const nr = hole[0] + neighbour[0];
+            if (nr < 0 || nr >= width) {
+                continue;
+            }
+
+            const nc = hole[1] + neighbour[1];
+            if (nc < 0 || nc >= height) {
+                continue;
+            }
+
+            if (mask.data[ (nr*width) + nc ] >= 0.5) {
+                filled_image.data[ (nr*width) + nc ] = 0;
+                boundaries.add([nr, nc, filled_image.data[ (nr*width) + nc ]]);
             }
         }
     }
 
-    console.log(holes.length);
+    console.log(boundaries.size);
 
     // Write out the image
     await sharp(filled_image.data, { raw: { width, height, channels } })
@@ -39,5 +62,48 @@ async function processImage(imagePath: string, maskPath: string) {
             .toFile('.\\resources\\nature_filled.png');
 }
 
+function test() {
+    let arr = [ [1,1,1,1,1],
+                [1,1,0,1,1],
+                [1,1,0,1,1],
+                [1,1,1,1,1],
+                [1,1,1,1,1] ];
+
+    let holes = [];
+    for (let i=0; i<arr.length; i++) {
+        for (let j=0; j<arr[i].length; j++) {
+            if (arr[i][j] === 0) {
+                holes.push([i,j]);
+            }
+        }
+    }
+
+    console.log(holes);
+
+    let boundaries = new Set();
+    const neighbours = [[-1,0], [1,0], [0,-1], [0,1]];
+    for (let hole of holes) {
+        for (let neighbour of neighbours) {
+            const nr = hole[0] + neighbour[0];
+            if (nr < 0 || nr >= arr.length) {
+                continue;
+            }
+
+            const nc = hole[1] + neighbour[1];
+            if (nc < 0 || nc >= arr[0].length) {
+                continue;
+            }
+
+            if (arr[nr][nc] != 0) {
+                boundaries.add([nr, nc, arr[nr][nc]]);
+            }
+        }
+    }
+
+    console.log(boundaries);
+
+}
+
 console.log("Hello World!");
 processImage(".\\resources\\nature.png", ".\\resources\\nature_mask.png");
+// test();
